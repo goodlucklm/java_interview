0bfacb5eda6936f8ad02e8409834432417af61a3
minglu <goodlucklm@gmail.com>
2017-12-05 13:41:26 -0800

    solve soduku, the slow solution, 121ms, 4.73%

--- src/SodukuQuestions.java
+++ src/SodukuQuestions.java
@@ -40,19 +40,46 @@ public class SodukuQuestions {
         return true;
     }
 
+    public boolean solveMySudoku(char[][] board) {
+        if (!isValidSudoku(board))
+            return false;
+        boolean matched = false;
+        int n = board.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == '.') {
+                    for (char ch = '1'; ch <= '9'; ch++ ) {
+                        board[i][j] = ch;
+                        if (solveMySudoku(board))
+                            return true;
+                    }
+                    board[i][j] = '.';
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void solveSudoku(char[][] board) {
+        solveMySudoku(board);
+        return;
+    }
+
     public static void main(String[] args) {
         char[][] board = {
-                {'.', '.', '4', '.', '.', '.', '6', '3', '.'},
-                {'.', '.', '.', '.', '.', '.', '.', '.', '.'},
-                {'5', '.', '.', '.', '.', '.', '.', '9', '.'},
-                {'.', '.', '.', '5', '6', '.', '.', '.', '.'},
-                {'4', '.', '3', '.', '.', '.', '.', '.', '.'},
-                {'.', '.', '.', '7', '.', '.', '.', '.', '.'},
-                {'.', '.', '.', '5', '.', '.', '.', '.', '.'},
-                {'.', '.', '.', '.', '.', '.', '.', '.', '.'},
-                {'.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'},
         };
         SodukuQuestions sq = new SodukuQuestions();
+        sq.solveSudoku(board);
         System.out.println("your result is: "+sq.isValidSudoku(board));
     }
 }
